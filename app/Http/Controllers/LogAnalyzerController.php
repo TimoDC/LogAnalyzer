702a6751d6c6dcf21ecc458f4d10d2c46e9a6e62
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-03-22 10:43:23 +0100

    Add main

--- app/Http/Controllers/LogAnalyzerController.php
+++ app/Http/Controllers/LogAnalyzerController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class LogAnalyzerController extends Controller
 {
     function index(){
-        return view("master");
+        return view("main");
     }
 }
---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+@extends("master")
+
+@section("main")
+<section>
+    <ul>
+        <li>Apache Logs</li>
+        <li>SSH Logs</li>
+        <li>ModSec Logs</li>
+        <li>MySQL Logs</li>
+        <li>Auth Logs</li>
+        <li>Overview All Logs</li>
+    </ul>
+</section>
+@endsection
