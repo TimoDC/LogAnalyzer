88432ef65ac2d91659c5fcc04d59da1943bc05d8
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-03-22 15:07:45 +0100

    Get aside top left

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -21,19 +21,5 @@
         </div>
     </ul>
 </section>
-<aside>
-    <a href="#" id="newDashboardButton">New Dashboard</a>
-
-    <div id="dashboardButtons">
-        <a href="#">Dashboard 1</a>
-    
-        <a href="#">Dashboard 2</a>
-    
-        <a href="#">Dashboard 3</a>
-    </div>
-
-    <a href="#" id="logoutButton">Logout</a>
-    <a href="#" id="settingsButton">Settings</a>
-</aside>
 
 @endsection
