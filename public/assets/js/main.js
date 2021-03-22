5e2f76e1f9a068d7402412e6f16383f92de17006
debian <debian@debian.local>
2021-03-22 17:59:06 +0100

    able to open and close popup

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 document.querySelector("#newDashboardButton").addEventListener("click", openNewDashboardPopup);
+document.querySelector(".close").addEventListener("click", closeNewDashboardPopup);
 
 function openNewDashboardPopup() {
     console.log("werkt");
@@ -8,8 +9,19 @@ function openNewDashboardPopup() {
     makeBackgroundTransparent();
 }
 
+function closeNewDashboardPopup() {
+    document.querySelector(".new-dashboard-popup").classList.add("hidden");
+    removeBackgroundTransparency();
+}
+
+
 function makeBackgroundTransparent() {
     const containerSharesInfo = document.querySelector("main");
     containerSharesInfo.classList.add("transparency");
     console.log("werkt")
+}
+
+function removeBackgroundTransparency() {
+    const containerSharesInfo = document.querySelector("main");
+    containerSharesInfo.classList.remove("transparency");
 }
