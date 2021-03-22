5741f460b476dc4384ea5f77de5b0b8916609470
debian <debian@debian.local>
2021-03-22 10:40:18 +0100

    Hardcoded dashboards buttons + logout and settings button

---
+++ resources/views/aside.blade.php
@@ -0,0 +1,12 @@
+<aside>
+    <h3>Dashboards: </h3>
+
+    <h4><a href=#>Dashboard 1</a></h4>
+    
+    <h4><a href=#>Dashboard 2</a></h4>
+    
+    <h4><a href=#>Dashboard 3</a></h4>
+
+    <a href=#>Logout</a>
+    <a href=#>Settings</a>
+</aside>
