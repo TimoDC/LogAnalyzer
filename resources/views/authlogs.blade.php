b3cf3c3d6f8fb158f81af5ed2a14b903ca55114f
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-03-23 10:38:06 +0100

    Add upload form + route

--- resources/views/authlogs.blade.php
+++ resources/views/authlogs.blade.php
@@ -1,7 +1,20 @@
 @extends("master")
 
+@section("css")
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/authlogs.css') }}"/>
+@endsection
+
 @section("main")
 <section>
     <h1>Auth Logs</h1>
+
+    <form method="post" action="{{ route('upload-authlog') }}">
+        @csrf
+
+        <label for="authlog">Upload Auth Log</label>
+        <input type="file" name="authlog" id="authlog" accept=".log" required>
+
+        <input type="submit" name="upload" value="Upload">
+    </form>
 </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes(['register' => false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/authlogs', 'AuthLogsController@index') -> name('authlogs');
+Route::post('/authlogs/uploaded', 'AuthLogsController@upload') -> name('upload-authlog');
