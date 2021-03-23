832a653d4e28706cbaef6413c8368f61dde6097a
debian <debian@debian.local>
2021-03-23 14:08:44 +0100

    added css to table

--- resources/views/memory.blade.php
+++ resources/views/memory.blade.php
@@ -7,7 +7,8 @@
     <title>Memory Usage</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/reset.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/screen.css') }}"/>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/memory.css') }}"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"
             integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw=="
             crossorigin="anonymous"></script>
 </head>
