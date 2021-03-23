11bad650cb7cb15ce170cf41a04742a60e423e2a
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-03-23 10:37:23 +0100

    Add upload & validation

--- app/Http/Controllers/AuthLogsController.php
+++ app/Http/Controllers/AuthLogsController.php
@@ -9,4 +9,20 @@ class AuthLogsController extends Controller
     function index() {
         return view("authlogs");
     }
+
+    function upload(Request $request) {
+        $data = $this -> validation($request);
+
+        $authlog = $data["authlog"] -> store("authlog");
+
+        return view("authlog-uploaded");
+    }
+
+    function validation(Request $request) {
+        $rules = [
+            "authlog" => "required|mimes:log"
+        ];
+
+        return $request -> validate($rules);
+    }
 }
