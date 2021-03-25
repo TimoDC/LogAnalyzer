2107e7e44a7351fd9c7f45d442574c98d0eb1c7a
debian <debian@debian.local>
2021-03-25 10:26:50 +0100

    reformat code

--- resources/views/memory.blade.php
+++ resources/views/memory.blade.php
@@ -5,6 +5,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>
 @stop
 
+
 @section('php')
 <?php
 $MemTotal = shell_exec("free --mega |grep Mem| cut -c 10-22 | sed -e 's/^[ \t]*//'");
@@ -36,111 +37,109 @@ $cpuSpeed = shell_exec("cat /proc/cpuinfo | grep name | awk '{print $7,$8,$9'}")
 
 
 @section('content')
-    <div id="CanvasContainer">
-        <div>
-            <canvas id="memoryCanvas"></canvas>
-        </div>
-
-        <div>
-            <canvas id="swapCanvas"></canvas>
-        </div>
-
-        <div>
-            <canvas id="cpuCanvas"></canvas>
-        </div>
-
-        <div>
-            <canvas id="diskCanvas"></canvas>
-        </div>
+<div id="CanvasContainer">
+    <div>
+        <canvas id="memoryCanvas"></canvas>
     </div>
 
-    <div class="hidden" id="tableContainer">
-        <table>
-            <tr>
-                <th></th>
-                <th scope="col">total</th>
-                <th scope="col">used</th>
-                <th scope="col">free</th>
-            </tr>
-            <tr>
-                <th scope="row">Mem</th>
-                <td class="MemTotal"><?php echo $MemTotal ?>MB</td>
-                <td id="MemUsed"><?php echo $MemUsed ?>MB</td>
-                <td id="MemFree"><?php echo $MemFree ?>MB</td>
-            </tr>
-            <tr>
-                <th scope="row">Swap</th>
-                <td class="SwapTotal"><?php echo $SwapTotal ?>MB</td>
-                <td id="SwapUsed"><?php echo $SwapUsed ?>MB</td>
-                <td id="SwapFree"><?php echo $SwapFree ?>MB</td>
-            </tr>
-        </table>
-
-        <table>
-            <tr>
-                <th scope="row">Amount of running processes</th>
-                <td id="MemFree"><?php echo $AmountProcesses ?></td>
-            </tr>
-        </table>
-
-        <table>
-            <tr>
-                <th scope="row">CPU (user)</th>
-                <td id="user"><?php echo $usr ?>%</td>
-            </tr>
-            <tr>
-                <th scope="row">CPU (system)</th>
-                <td id="system"><?php echo $sys ?>%</td>
-            </tr>
-            <tr>
-                <th scope="row">CPU (iowait)</th>
-                <td id="iowait"><?php echo $iowait ?>%</td>
-            </tr>
-            <tr>
-                <th scope="row">CPU (idle)</th>
-                <td id="idle"><?php echo $idle ?>%</td>
-            </tr>
-        </table>
-
-        <table>
-            <tr>
-                <th></th>
-                <th scope="col">total</th>
-                <th scope="col">used</th>
-                <th scope="col">free</th>
-            </tr>
-            <tr>
-                <th scope="row"><?php echo $DiskName ?></th>
-                <td class="DiskTotal"><?php echo $DiskTotal ?>GB</td>
-                <td id="DiskUsed"><?php echo $DiskUsed ?>GB</td>
-                <td id="DiskFree"><?php echo $DiskFree ?>GB</td>
-            </tr>
-        </table>
-
-        <table>
-            <tr>
-                <th scope="row">Hostname</th>
-                <td id="user"><?php echo $hostname ?></td>
-            </tr>
-            <tr>
-                <th scope="row">Kernel Release</th>
-                <td id="system"><?php echo $kernelRelease ?></td>
-            </tr>
-            <tr>
-                <th scope="row">CPU Model</th>
-                <td id="iowait"><?php echo $cpuModel ?></td>
-            </tr>
-            <tr>
-                <th scope="row">CPU Speed</th>
-                <td id="idle"><?php echo $cpuSpeed ?></td>
-            </tr>
-        </table>
+    <div>
+        <canvas id="swapCanvas"></canvas>
     </div>
 
-@stop
+    <div>
+        <canvas id="cpuCanvas"></canvas>
+    </div>
 
-    @section('script')
-    <script src="{{ asset('assets/js/memory.js') }}"></script>
+    <div>
+        <canvas id="diskCanvas"></canvas>
+    </div>
+</div>
+
+<div class="hidden" id="tableContainer">
+    <table>
+        <tr>
+            <th></th>
+            <th scope="col">total</th>
+            <th scope="col">used</th>
+            <th scope="col">free</th>
+        </tr>
+        <tr>
+            <th scope="row">Mem</th>
+            <td class="MemTotal"><?php echo $MemTotal ?>MB</td>
+            <td id="MemUsed"><?php echo $MemUsed ?>MB</td>
+            <td id="MemFree"><?php echo $MemFree ?>MB</td>
+        </tr>
+        <tr>
+            <th scope="row">Swap</th>
+            <td class="SwapTotal"><?php echo $SwapTotal ?>MB</td>
+            <td id="SwapUsed"><?php echo $SwapUsed ?>MB</td>
+            <td id="SwapFree"><?php echo $SwapFree ?>MB</td>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <th scope="row">Amount of running processes</th>
+            <td id="MemFree"><?php echo $AmountProcesses ?></td>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <th scope="row">CPU (user)</th>
+            <td id="user"><?php echo $usr ?>%</td>
+        </tr>
+        <tr>
+            <th scope="row">CPU (system)</th>
+            <td id="system"><?php echo $sys ?>%</td>
+        </tr>
+        <tr>
+            <th scope="row">CPU (iowait)</th>
+            <td id="iowait"><?php echo $iowait ?>%</td>
+        </tr>
+        <tr>
+            <th scope="row">CPU (idle)</th>
+            <td id="idle"><?php echo $idle ?>%</td>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <th></th>
+            <th scope="col">total</th>
+            <th scope="col">used</th>
+            <th scope="col">free</th>
+        </tr>
+        <tr>
+            <th scope="row"><?php echo $DiskName ?></th>
+            <td class="DiskTotal"><?php echo $DiskTotal ?>GB</td>
+            <td id="DiskUsed"><?php echo $DiskUsed ?>GB</td>
+            <td id="DiskFree"><?php echo $DiskFree ?>GB</td>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <th scope="row">Hostname</th>
+            <td id="user"><?php echo $hostname ?></td>
+        </tr>
+        <tr>
+            <th scope="row">Kernel Release</th>
+            <td id="system"><?php echo $kernelRelease ?></td>
+        </tr>
+        <tr>
+            <th scope="row">CPU Model</th>
+            <td id="iowait"><?php echo $cpuModel ?></td>
+        </tr>
+        <tr>
+            <th scope="row">CPU Speed</th>
+            <td id="idle"><?php echo $cpuSpeed ?></td>
+        </tr>
+    </table>
+</div>
 @stop
 
 
+@section('script')
+<script src="{{ asset('assets/js/memory.js') }}"></script>
+@stop
